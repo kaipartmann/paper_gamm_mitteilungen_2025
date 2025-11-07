6c87de40be0bea587b81b346876d34be9270963b
Kai Partmann <kai.partmann@gmail.com>
2025-11-07 08:49:50 +0100

    Add setup file

---
+++ setup.jl
@@ -0,0 +1,9 @@
+# Setup the Julia environment for the Peridynamics project
+import Pkg
+Pkg.activate(@__DIR__)
+
+# Install Peridynamics package specified inside the Project.toml
+Pkg.instantiate()
+
+# Install specific version of Peridynamics from GitHub
+Pkg.add(Pkg.PackageSpec(name="Peridynamics", rev="rkc_enhancements"))
